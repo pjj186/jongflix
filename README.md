a84c85bc7ef9185a263ce75975bbb9927e4d29ce
Jong jae Park <pjj186@naver.com>
2021-08-19 19:41:37 +0900

    API Verbs
    axios를 이용해서 get 요청 보내기
    함수는 그에 대한 response를 리턴

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 - [ ] Detail
 
 ## API Verbs
+
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Popular (TV, Movie)
+- [x] Top Rated (TV)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (TV, Movie)
---
+++ src/api.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "0c6f3c84d4564ed4f2afcb7aa3744089",
+    language: "en-US",
+  },
+});
+
+// 상대 경로 사용에 유의! 절대 경로를 쓰게 되면 baseURL을 덮어씌우게된다.
+
+export const moviesApi = {
+  noewPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcomming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        query: term,
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: term,
+      },
+    }),
+};
