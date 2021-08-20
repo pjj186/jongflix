673ec516693dd18b21fb0cc3e2fe66f01542a66e
Jong jae Park <pjj186@naver.com>
2021-08-20 13:49:34 +0900

    SearchContainer

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Detail from "Routes/Detail";
 import Search from "Routes/Search";
 import Header from "Components/Header";
 
@@ -15,6 +16,8 @@ function Router() {
           <Route path="/" exact component={Home} />
           <Route path="/tv" exact component={TV} />
           <Route path="/search" component={Search} />
+          <Route path="/movie/:id" component={Detail} />
+          <Route path="/show/:id" component={Detail} />
           <Redirect from="*" to="/" />
         </Switch>
       </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 
@@ -10,6 +11,35 @@ class SearchContainer extends React.Component {
     error: null,
   };
 
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      // searchTerm이 공백이 아닐 때, searchByTerm을 호출
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+      });
+    } catch (error) {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
     return (
@@ -19,6 +49,7 @@ class SearchContainer extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
