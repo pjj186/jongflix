aee0d04095f4a92271bcb5cdc9bf5226e9cc561c
Jong jae Park <pjj186@naver.com>
2021-08-20 18:30:31 +0900

    HomePresenter & Section
    주제별로 섹션을 나누고, 스타일 작업

---
+++ src/Components/Section.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const Grid = styled.div`
+  margin-top: 25px;
+`;
+
+// children은 예약어인 react-prop임
+const Section = ({ title, children }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Grid>{children}</Grid>
+    </Container>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
