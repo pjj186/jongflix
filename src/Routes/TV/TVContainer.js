e22e6e3c3ebbc639a0b6a014b2eb94c01204d129
Jong jae Park <pjj186@naver.com>
2021-08-20 11:49:01 +0900

    TV Container
    Home Container 와 같은 방법이다.

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,3 +1,4 @@
+import { tvApi } from "api";
 import React from "react";
 import TVPresenter from "./TVPresenter";
 
@@ -9,6 +10,32 @@ class TVContainer extends React.Component {
     loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch (error) {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
