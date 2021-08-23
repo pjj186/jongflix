174088350274f186f3d829213bed6b62416074d7
Jong jae Park <pjj186@naver.com>
2021-08-23 15:40:33 +0900

    로딩 애니메이션
    로딩 애니메이션 추가

--- note.md
+++
@@ -1,3 +0,0 @@
-Redirect : 일치하는 URL이 없으면 to에 설정한 URL로 이동
-<br>
-Switch : 한 번에 오직 하나의 Route만 Render하게 해준다.
--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,5 +1,35 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const Ringanim = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LdsRing = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+`;
+
+const InLdsRing = styled.div`
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: ${Ringanim} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  animation-delay: ${(props) => props.delay};
+  border-color: #fff transparent transparent transparent;
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -12,9 +42,11 @@ const Container = styled.div`
 
 const Loader = () => (
   <Container>
-    <span role="img" aria-label="Loading">
-      ⏰
-    </span>
+    <LdsRing>
+      <InLdsRing delay="-0.45s"></InLdsRing>
+      <InLdsRing delay="-0.3s"></InLdsRing>
+      <InLdsRing delay="-0.15s"></InLdsRing>
+    </LdsRing>
   </Container>
 );
 
